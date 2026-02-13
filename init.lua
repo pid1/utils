9b82252fec099eebbaaf6e21d2278211eb97e99c
Jonathan Roemer <jon@roemersoftworks.com>
2026-02-13 14:21:55 -0600

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,27 @@
+-- Neovim config
+
+vim.opt.number = true              -- Show line numbers
+vim.opt.ignorecase = true          -- Case insensitive search
+vim.opt.hlsearch = true            -- Highlight search results
+vim.opt.incsearch = true           -- Incremental search
+vim.opt.wrap = true                -- Wrap lines
+vim.opt.breakindent = true         -- Preserve indentation in wrapped text
+vim.opt.tabstop = 2                -- Tab width
+vim.opt.shiftwidth = 2             -- Indent width
+vim.opt.expandtab = true           -- Use spaces instead of tabs
+vim.opt.smartindent = true         -- Smart autoindenting
+vim.opt.termguicolors = true       -- Use terminal colors
+vim.opt.signcolumn = 'yes'         -- Always show sign column
+vim.opt.splitright = true          -- Vertical splits to the right
+vim.opt.splitbelow = true          -- Horizontal splits below
+vim.opt.undofile = true            -- Persistent undo
+vim.opt.backup = false             -- No backup files
+vim.opt.writebackup = false        -- No backup before overwriting
+vim.opt.swapfile = false           -- No swap files
+vim.opt.scrolloff = 8              -- Lines to keep above/below cursor
+vim.opt.sidescrolloff = 8          -- Columns to keep left/right of cursor
+vim.opt.cursorline = true          -- Highlight current line
+vim.opt.showmode = true            -- Show mode
+
+vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
+vim.cmd('highlight NonText guibg=NONE ctermbg=NONE')
